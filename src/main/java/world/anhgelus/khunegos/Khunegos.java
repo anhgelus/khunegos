25d3b00717970bfb15dd6c04fa09039e5e0ed28f
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-09-29 16:38:54 +0200

    Initial commit

---
+++ src/main/java/world/anhgelus/khunegos/Khunegos.java
@@ -0,0 +1,10 @@
+package world.anhgelus.khunegos;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Khunegos implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
